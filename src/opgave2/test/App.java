7ee2422cac64c9252b017c4f161e45f866afbdb8
Bal1le <christian.arendt.daniel@gmail.com>
2024-10-02 11:39:20 +0200

    Opgave1 færdig

--- src/opgave2/test/App.java
+++ src/opgave2/test/App.java
@@ -2,9 +2,13 @@ package opgave2.test;
 
 import opgave2.model.Student;
 
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
         // Create students
         Student martin = new Student("Martin");
+
+
     }
 }
