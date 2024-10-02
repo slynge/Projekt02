4857ea1608c0c20032c948779cb50e3a2bb8969b
Bal1le <christian.arendt.daniel@gmail.com>
2024-10-02 11:10:02 +0200

    Gjort klar til opgave2

--- src/opgave1/test/App.java
+++ src/opgave1/test/App.java
@@ -6,5 +6,14 @@ public class App {
     public static void main(String[] args) {
         // Create students
         Student martin = new Student("Martin");
+        Student torben = new Student("Torben");
+        Student emil = new Student("Emil");
+        Student christian = new Student("Christian");
+        Student bo = new Student("bo");
+        Student suzuki = new Student("Suzuki");
+
+
+
+
     }
 }
---
+++ src/opgave2/test/App.java
@@ -0,0 +1,10 @@
+package opgave2.test;
+
+import opgave2.model.Student;
+
+public class App {
+    public static void main(String[] args) {
+        // Create students
+        Student martin = new Student("Martin");
+    }
+}
