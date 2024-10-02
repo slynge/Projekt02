0b586422a461f295f838223855fd42e483e7e00d
EmilStoeve <emilstoeve@icloud.com>
2024-10-02 11:08:03 +0200

    Updatering

--- src/model/Student.java
+++ src/model/Student.java
@@ -11,20 +11,19 @@ public class Student {
     }
 
 
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public boolean isActive() {
         return active;
     }
 
-    public void setActive(boolean active) {
-        this.active = active;
-    }
+
 }
