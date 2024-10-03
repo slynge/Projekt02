b13c6dca0e4c8ef4d4638b500910b0a0be27ea68
Martin Slyngborg <slyngborgmartin@gmail.com>
2024-10-03 09:47:54 +0200

    app

--- src/opgave2/test/App.java
+++ src/opgave2/test/App.java
@@ -9,12 +9,12 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) {
         // Create students
-        opgave1.model.Student martin = new opgave1.model.Student("Martin");
-        opgave1.model.Student torben = new opgave1.model.Student("Torben");
-        opgave1.model.Student emil = new opgave1.model.Student("Emil");
-        opgave1.model.Student christian = new opgave1.model.Student("Christian");
-        opgave1.model.Student bo = new opgave1.model.Student("Bo");
-        opgave1.model.Student suzuki = new Student("Suzuki");
+        Student martin = new Student("Martin");
+        Student torben = new Student("Torben");
+        Student emil = new Student("Emil");
+        Student christian = new Student("Christian");
+        Student bo = new Student("Bo");
+        Student suzuki = new Student("Suzuki");
 
         Team team1 = new Team("Team1","Rum6000", new ArrayList<>());
         Team team2 = new Team("Team2","Rum808", new ArrayList<>());
