3110db1dc1574c1c80bcf710900acf5c50540ea6
EmilStoeve <emilstoeve@icloud.com>
2024-10-04 09:18:02 +0200

    Update - App3b

--- src/opgave2/test/App3b.java
+++ src/opgave2/test/App3b.java
@@ -7,21 +7,44 @@ import opgave2.model.MultipleChoice;
 import java.util.Scanner;
 
 public class App3b {
+
+    private boolean systemStatus; // default false
+
     public static void main(String[] args) {
+        greeting();
+        requestUserInteraction();
+
+        int userSystemStatus = intUserInput();;
+
+        while (systemStatus(userSystemStatus)){
 
 
+            // System requests user interaction
+            requestUserInteraction();
+            userSystemStatus = intUserInput();
+        }
 
     }
 
-    private static void appUImenu(){
+    private static void greeting(){
+        System.out.println("===========================================");
+        System.out.println("Welcome to a educational management system");
+        System.out.println("===========================================");
+    }
+    private static void requestUserInteraction(){
+        System.out.print("Press 0 to start system: ");
+    }
+    private static boolean systemStatus(int userInput){
+        return (userInput == 0);
+    }
 
+    private static void menuUI(){
         System.out.println("1: Create a team");
         System.out.println("2: Create a student");
         System.out.println("3: Show one student's info and results");
         System.out.println("4: Show one  team's info and results");
         System.out.println("5: Show info and results for all teams");
         System.out.println("6: Exit program");
-
     }
 
     private static int intUserInput(){
