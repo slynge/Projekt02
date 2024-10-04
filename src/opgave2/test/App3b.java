fdae63df362a948c4e6bbdf9b740b9a52232c055
Bal1le <christian.arendt.daniel@gmail.com>
2024-10-04 08:57:52 +0200

    UI Menu

--- src/opgave2/test/App3b.java
+++ src/opgave2/test/App3b.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class App3b {
     public static void main(String[] args) {
 
+
+
+    }
+
+    public void UIMenu(){
+
+        System.out.println("1: Create a team");
+        System.out.println("2: Create a student");
+        System.out.println("3: Show one student's info and results");
+        System.out.println("4: Show one  team's info and results");
+        System.out.println("5: Show info and results for all teams");
+        System.out.println("6: Exit program");
+
     }
 
     private static int intUserInput(){
