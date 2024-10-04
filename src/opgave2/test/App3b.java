b9c0509fec6820197c003548533d7b2891b88d65
EmilStoeve <emilstoeve@icloud.com>
2024-10-04 08:57:00 +0200

    Update - App3b

--- src/opgave2/test/App3b.java
+++ src/opgave2/test/App3b.java
@@ -1,7 +1,32 @@
 package opgave2.test;
 
+import java.util.Scanner;
+
 public class App3b {
     public static void main(String[] args) {
 
     }
+
+    private static int intUserInput(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+    private static String stringUserInput(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    switch (inputNumber){
+        case -3:
+        case 0:
+        case 2:
+        case 4:
+        case 7:
+        case 10:
+        case 12:
+            return true;
+        default:
+            return false;
+    }
+
 }
